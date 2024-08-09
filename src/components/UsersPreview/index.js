893bccb6586d17450cfd941e426a7b6227736dd5
Richard Alves <richard.alves1994@gmail.com>
2024-08-08 22:48:19 -0300

    feat (connections): add modal to all connections

--- src/components/UsersPreview/index.js
+++ src/components/UsersPreview/index.js
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import "./styles.css";
 import Image from "next/image";
+import { useState } from "react";
+import Modal from "../Modal";
 
 export default function UsersPreview({ title, users }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <section className="usersPreviewContainer">
       <div className="usersPreviewTexts">
         <span>
           {title} ({users.length})
         </span>
-        <span className="cursorPointer" style={{ fontSize: 11 }}>
+        <span
+          className="cursorPointer"
+          style={{ fontSize: 11 }}
+          onClick={() => setIsModalOpen(true)}
+        >
           Ver todos
         </span>
       </div>
@@ -30,6 +37,24 @@ export default function UsersPreview({ title, users }) {
           }
         })}
       </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <div className="allConnectionsPicturesContainer">
+          {users.map((user, index) => {
+            return (
+              <Link key={user.id} href={`/${user.id}`} className="connectionLink ">
+                <Image
+                  alt={user.name}
+                  src={user.photo}
+                  className="allConnectionsPicture cursorPointer"
+                  width={90}
+                  height={90}
+                />
+                <span>{user.name}</span>
+              </Link>
+            );
+          })}
+        </div>
+      </Modal>
     </section>
   );
 }
